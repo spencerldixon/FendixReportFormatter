a511d996f7209d2c032913fd1dd29d04a8e0c192
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-09-02 16:18:40 +0100

    Added graph for impressions, need to work on query to label sites

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def fullname user
+		"#{user.first_name} #{user.last_name}"
+	end
 end
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,8 @@
 module ReportsHelper
+	def get_impressions_array report
+		hash = report.line_items.group(:site_id).sum(:impressions)
+		array_headers = ['Site', 'Impressions']
+		array = hash.to_a
+		array.unshift(array_headers)
+	end
 end
