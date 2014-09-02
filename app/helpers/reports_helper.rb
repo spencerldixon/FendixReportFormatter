26943505b58b7ba112c0e0bc2dc45c17af058ff6
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-09-02 17:20:43 +0100

    Added graphs and query is wasteful but works

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -2,7 +2,33 @@ module ReportsHelper
 	def get_impressions_array report
 		hash = report.line_items.group(:site_id).sum(:impressions)
 		array_headers = ['Site', 'Impressions']
-		array = hash.to_a
+		new_hash = hash.map do |k, v|
+			site = Site.find(k)
+			k = site.name, v
+		end
+		array = new_hash.to_a
+		array.unshift(array_headers)
+	end
+
+	def get_clicks_array report
+		hash = report.line_items.group(:site_id).sum(:clicks)
+		array_headers = ['Site', 'Clicks']
+		new_hash = hash.map do |k, v|
+			site = Site.find(k)
+			k = site.name, v
+		end
+		array = new_hash.to_a
+		array.unshift(array_headers)
+	end
+
+	def get_ctr_array report
+		hash = report.line_items.group(:site_id).sum(:ctr)
+		array_headers = ['Site', 'CTR']
+		new_hash = hash.map do |k, v|
+			site = Site.find(k)
+			k = site.name, v
+		end
+		array = new_hash.to_a
 		array.unshift(array_headers)
 	end
 end
