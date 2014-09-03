796b42667d9ef324d9160f87f5bb61d54512deb3
Spencer Dixon <spencerlloyddixon@gmail.com>
2014-09-03 14:04:46 +0100

    Added warning message if records start to get closer to herokus limit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,9 @@ module ApplicationHelper
 	def absolute_image_tag(*args)
 	  raw(image_tag(*args).sub /src="(.*?)"/, "src=\"#{request.protocol}#{request.host_with_port}" + '\1"')
 	end
+
+	def database_count
+		LineItem.count + Report.count + User.count + Site.count
+	end
 end
 
